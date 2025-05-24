f07a4e80b65fa667dcfe6115e9b31bc602a0338a
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-05-24 10:46:43 +0300

    Close menu when the nav link is clicked

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.getElementById("menu-open-button");
 const menuCloseButton = document.getElementById("menu-close-button");
 
 menuOpenButton.addEventListener('click', ()=> {
+    // Toggle mobile menu visible
     document.body.classList.toggle("show-mobile-menu");
 });
 
+
+// Close menu when the close button is clicked
 menuCloseButton.addEventListener('click', () => menuOpenButton.click());
 
+//Close menu when the nav link is clicked
+
+navLinks.forEach(link => {
+    link.addEventListener('click', ()=>{
+        menuOpenButton.click()
+    });
+});
 
 // Initialize swiper from their website
 const swiper = new Swiper('.slider-wrapper', {
